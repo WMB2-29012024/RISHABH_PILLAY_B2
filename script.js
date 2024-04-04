b0969a2aedca01da3d258e99667e6d3d280dc72f
Rishabh Pillay <rishabhpillay03@gmail.com>
2024-04-04 16:24:21 +0530

    input_name_email

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const signUpFormElem = document.getElementById('sign_up_form');
 
-signUpFormElem.addEventListener('submit', (event)=>{
+signUpFormElem.addEventListener('submit', (event) => {
     event.preventDefault();
     const userName = event.target.querySelector('#user_name').value;
     const userEmail = event.target.querySelector('#user_email').value;
-    console.log(`Full Name: ${userName}`);
-    console.log(`Email: ${userEmail}`);
+    const userPassword = event.target.querySelector('#user_password').value;
+    const userConfirmPassword = event.target.querySelector('#user_confirm_password').value;
+
+    if (userPassword === userConfirmPassword) {
+
+        window.alert('Form submitted');
+
+    } else {
+
+        const errorMessageElem = document.querySelector('#error_message');
+        errorMessageElem.innerHTML = 'Please enter same password';     
+
+    }
+
+
+
 });
