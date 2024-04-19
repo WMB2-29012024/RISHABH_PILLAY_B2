e25c92f1ecce53efcfe57c0acccd02bc612c5e7e
Rishabh Pillay <rishabhpillay03@gmail.com>
2024-04-19 18:34:03 +0530

    D5

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const hamburgerElem = document.querySelector('.hamburger')
-const asideElem = document.querySelector('aside')
-const asideAElem = document.querySelectorAll('aside a')
-
+const hamburgerElem = document.querySelector('.hamburger');
+const asideElem = document.querySelector('aside');
+const asideAElems = document.querySelectorAll('aside a');
 
 let click = 0;
 
-hamburgerElem.addEventListener('click', (e)=>{
-
-    if(click === 0){
-        
-        asideElem.style.width = "300px";
-        // asideAElem.style.display="initial"
+hamburgerElem.addEventListener('click', () => {
+    if (click === 0) {       
         click = 1;
         hamburgerElem.innerHTML = '✕';
-    }else{
-        asideElem.style.width= "0px";
-        // asideAElem.style.display="none"
-        // asideElem.style.display="none"
-        click = 0
-        hamburgerElem.innerHTML = '☰';
-    }    
+        asideElem.style.width='300px'        
+        asideAElems.forEach(elem => {
+            elem.style.display = "block";
+          });
 
-})
+
+    } else {
+        click = 0;
+        hamburgerElem.innerHTML = '☰';
+        asideAElems.forEach(elem => {
+            elem.style.display = "none";
+          });
+
+          asideElem.style.width='0px'
+    }
+});
+
+
+
